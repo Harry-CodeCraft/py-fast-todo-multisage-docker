3e397f03d7eb1bb78eec14f400f2ed57cfe13fff
harsh singh <harshsingh@omniesolutions.com>
2025-08-07 16:21:40 +0530

    added k8s yml menifest files

--- app/database/db_engine_session.py
+++ app/database/db_engine_session.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DOCKER_VOLUME_DATABASE_URL = "postgresql+asyncpg://postgres:root@db:5433/py-fast-todo"
+DOCKER_VOLUME_DATABASE_URL = "postgresql+asyncpg://postgres:root@db:5432/py-fast-todo"
 NON_PROD_DATABASE_URL = "postgresql+asyncpg://postgres:root@localhost:5432/py-fast-todo"
 PROD_DATABASE_URL = "postgresql+asyncpg://postgres:root@192.168.1.72:5432/py-fast-todo"
 
-engine = create_async_engine(NON_PROD_DATABASE_URL, echo=True)
+engine = create_async_engine(DOCKER_VOLUME_DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False) # type: ignore
